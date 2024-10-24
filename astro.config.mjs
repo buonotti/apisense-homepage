07b4d97b1e1d3879e11e63f5f5710a8e457b6c4a
Aurelio Buonomo <48835871+auribuo@users.noreply.github.com>
2024-10-24 17:40:59 +0200

    Ported gitbook wiki

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,51 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import starlight from '@astrojs/starlight';
+
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    site: "https://apisense.dev",
+    integrations: [starlight({
+        title: 'Apisense',
+        editLink: {
+            baseUrl: "https://github.com/buonotti/apisense-homepage/edit/main/"
+        },
+        social: {
+            github: 'https://github.com/buonotti/apisense',
+        },
+        sidebar: [
+            {
+              label: "About Apisense",
+                link: "about"
+            },
+            {
+                label: "Get Started",
+                autogenerate: {directory: "get-started"},
+            },
+            {
+                label: 'Configuration',
+                autogenerate: {directory: 'configuration'},
+            },
+            {
+                label: 'Definitions',
+                autogenerate: {directory: 'definitions'},
+            },
+            {
+                label: 'Validators',
+                autogenerate: {directory: 'validators'},
+            },
+            {
+                label: 'Development',
+                autogenerate: {directory: 'development'},
+            },
+        ],
+        customCss: [
+            '@fontsource-variable/jetbrains-mono'
+        ],
+        components: {
+            Header: "./src/components/TopNav.astro"
+        }
+    }), tailwind()],
 });
