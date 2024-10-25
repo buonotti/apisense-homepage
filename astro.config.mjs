88cc9cf5f53b7684371cc6f4b944e5e93408add8
Aurelio Buonomo <48835871+auribuo@users.noreply.github.com>
2024-10-25 20:26:39 +0200

    Changed color scheme to catppuccin. Better theme selector

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,51 +1,58 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import {defineConfig} from "astro/config";
 import starlight from "@astrojs/starlight";
 import tailwind from "@astrojs/tailwind";
+import catppuccin from "starlight-theme-catppuccin";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://apisense.dev",
-  integrations: [
-    starlight({
-      title: "Apisense",
-      customCss: ["./src/styles/tailwind.css"],
-      editLink: {
-        baseUrl: "https://github.com/buonotti/apisense-homepage/edit/main/",
-      },
-      social: {
-        github: "https://github.com/buonotti/apisense",
-      },
-      sidebar: [
-        {
-          label: "About Apisense",
-          link: "about",
-        },
-        {
-          label: "Get Started",
-          autogenerate: { directory: "get-started" },
-        },
-        {
-          label: "Configuration",
-          autogenerate: { directory: "configuration" },
-        },
-        {
-          label: "Definitions",
-          autogenerate: { directory: "definitions" },
-        },
-        {
-          label: "Validators",
-          autogenerate: { directory: "validators" },
-        },
-        {
-          label: "Development",
-          autogenerate: { directory: "development" },
-        },
-      ],
-    }),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+    site: "https://apisense.dev",
+    integrations: [
+        starlight({
+            title: "Apisense",
+            customCss: ["./src/styles/tailwind.css"],
+            editLink: {
+                baseUrl: "https://github.com/buonotti/apisense-homepage/edit/main/",
+            },
+            social: {
+                github: "https://github.com/buonotti/apisense",
+            },
+            components: {
+              ThemeSelect: "./src/overrides/ThemeSelect.astro",
+            },
+            sidebar: [
+                {
+                    label: "About Apisense",
+                    link: "about",
+                },
+                {
+                    label: "Get Started",
+                    autogenerate: {directory: "get-started"},
+                },
+                {
+                    label: "Configuration",
+                    autogenerate: {directory: "configuration"},
+                },
+                {
+                    label: "Definitions",
+                    autogenerate: {directory: "definitions"},
+                },
+                {
+                    label: "Validators",
+                    autogenerate: {directory: "validators"},
+                },
+                {
+                    label: "Development",
+                    autogenerate: {directory: "development"},
+                },
+            ],
+            plugins: [
+                catppuccin({dark: "mocha-red", light: "latte-red"})
+            ]
+        }),
+        tailwind({
+            applyBaseStyles: false,
+        }), catppuccin()
+    ],
 });
 
