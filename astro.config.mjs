4e692340d7b96396a343e985ebe7de4fd26ff9b0
Aurelio Buonomo <48835871+auribuo@users.noreply.github.com>
2024-10-25 11:05:27 +0200

    Redid some content. Updated colors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,45 +1,51 @@
 // @ts-check
-import {defineConfig} from 'astro/config';
-import starlight from '@astrojs/starlight';
-
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://apisense.dev",
-    integrations: [starlight({
-        title: 'Apisense',
-        editLink: {
-            baseUrl: "https://github.com/buonotti/apisense-homepage/edit/main/"
+  site: "https://apisense.dev",
+  integrations: [
+    starlight({
+      title: "Apisense",
+      customCss: ["./src/styles/tailwind.css"],
+      editLink: {
+        baseUrl: "https://github.com/buonotti/apisense-homepage/edit/main/",
+      },
+      social: {
+        github: "https://github.com/buonotti/apisense",
+      },
+      sidebar: [
+        {
+          label: "About Apisense",
+          link: "about",
         },
-        social: {
-            github: 'https://github.com/buonotti/apisense',
+        {
+          label: "Get Started",
+          autogenerate: { directory: "get-started" },
         },
-        sidebar: [
-            {
-              label: "About Apisense",
-                link: "about"
-            },
-            {
-                label: "Get Started",
-                autogenerate: {directory: "get-started"},
-            },
-            {
-                label: 'Configuration',
-                autogenerate: {directory: 'configuration'},
-            },
-            {
-                label: 'Definitions',
-                autogenerate: {directory: 'definitions'},
-            },
-            {
-                label: 'Validators',
-                autogenerate: {directory: 'validators'},
-            },
-            {
-                label: 'Development',
-                autogenerate: {directory: 'development'},
-            },
-        ],
-    })],
+        {
+          label: "Configuration",
+          autogenerate: { directory: "configuration" },
+        },
+        {
+          label: "Definitions",
+          autogenerate: { directory: "definitions" },
+        },
+        {
+          label: "Validators",
+          autogenerate: { directory: "validators" },
+        },
+        {
+          label: "Development",
+          autogenerate: { directory: "development" },
+        },
+      ],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
+
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,30 @@
+import starlightPlugin from "@astrojs/starlight-tailwind";
+
+// Generated color palettes
+const accent = {
+  200: "#e2bdc6",
+  600: "#a64665",
+  900: "#4d232f",
+  950: "#351b22",
+};
+const gray = {
+  100: "#f6f6fa",
+  200: "#ecedf6",
+  300: "#c0c1cc",
+  400: "#89899e",
+  500: "#565669",
+  700: "#363648",
+  800: "#252535",
+  900: "#17171f",
+};
+
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', 'node_modules/preline/dist/*.js'],
-    theme: {
-        extend: {},
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: { accent, gray },
     },
-    plugins: [require("preline/plugin")],
-}
+  },
+  plugins: [starlightPlugin()],
+};
